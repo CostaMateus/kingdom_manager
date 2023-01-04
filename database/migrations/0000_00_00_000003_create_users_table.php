4522f9d2cb864244ca18246dc0822eb4c50197bf
CostaMateus <costa.mack95@gmail.com>
2023-01-04 20:45:28 -0300

    fix: na migration de users

--- database/migrations/0000_00_00_000003_create_users_table.php
+++ database/migrations/0000_00_00_000003_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create( "users", function ( Blueprint $table ) {
 
             $table->id();
-            $table->string( "name" );
+            $table->string( "nickname" )->unique();
             $table->string( "email" )->unique();
             $table->string( "password" );
             $table->string( "sex" )->default( "U" ); // Male, Female, Undefined
