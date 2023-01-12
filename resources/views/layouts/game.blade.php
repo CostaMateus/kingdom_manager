742969d9eef99834d11606b1219b38bd3c98d02f
CostaMateus <costa.mack95@gmail.com>
2023-01-12 08:34:43 -0300

    add: fonte logo

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -27,8 +27,8 @@
     <div id="app" >
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" >
             <div class="container" >
-                <a class="navbar-brand d-md-none" href="{{ route( "home" ) }}" >
-                    {{ config('app.name', 'Laravel') }}
+                <a id="km-menu" class="navbar-brand d-md-none" href="{{ route( "home" ) }}" >
+                    [{{ config('app.name', 'Laravel') }}]
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}" >
                     <span class="navbar-toggler-icon" ></span>
@@ -38,8 +38,8 @@
 
                     <div class="row mx-auto">
                         <div class="col-12 d-none d-md-block text-center">
-                            <a class="navbar-brand" href="{{ route( "home" ) }}" >
-                                {{ config('app.name', 'Laravel') }}
+                            <a id="km-main" class="navbar-brand mx-0" href="{{ route( "home" ) }}" >
+                                [{{ config('app.name', 'Laravel') }}]
                             </a>
                         </div>
                         <div class="col-12">
