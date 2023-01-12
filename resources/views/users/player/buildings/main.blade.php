7dae59ce2a9c33a9061d973335745c3fd9889827
CostaMateus <costa.mack95@gmail.com>
2023-01-12 14:12:03 -0300

    fix: ajuste na organizacao das colunas

--- resources/views/users/player/buildings/main.blade.php
+++ resources/views/users/player/buildings/main.blade.php
@@ -19,7 +19,7 @@
                         <div class="table-responsive" >
                             <table id="builded" class="table table-hover table-sm align-middle mb-0" >
                                 <thead>
-                                    <tr>
+                                    <tr class="text-center" >
                                         <th>Edifícios</th>
                                         <th>Requerimentos</th>
                                         <th>Construir</th>
@@ -30,10 +30,10 @@
                                         <tr>
                                             <td>
                                                 <div class="row" >
-                                                    <div class="col-3 text-center m-auto" >
+                                                    <div class="col-12 col-lg-4 text-center m-auto" >
                                                         <img src="{{ asset( "assets/graphic/buildings/{$key}1.png" ) }}" alt="" >
                                                     </div>
-                                                    <div class="col-9 ps-0" >
+                                                    <div class="col-12 col-lg-8 text-center text-lg-start ps-lg-0 m-auto" >
                                                         <a class="text-decoration-none text-dark" href="{{ route( "village.{$key}", [ "village" => $village ] ) }}" >
                                                             <p class="mb-0" >
                                                                 {{ $building[ "name" ] }}
@@ -52,24 +52,24 @@
                                             </td>
                                             <td>
                                                 <div class="row" >
-                                                    <div class="col-2 @if( $building[ "wood" ] > 100 ) text-danger @endif" title="{{ $buildings[ "wood" ][ "name" ] }}" >
+                                                    <div class="col-12 col-sm-4 col-lg-2" title="{{ $buildings[ "wood" ][ "name" ] }}" >
                                                         <img src="{{ asset( "assets/graphic/buildings/icons/{$buildings[ "wood" ][ "key" ]}.png" ) }}" alt="{{ $buildings[ "wood" ][ "name" ] }}" >
                                                         {{ $building[ "wood"       ] }}
                                                     </div>
-                                                    <div class="col-2 @if( $building[ "clay" ] > 100 ) text-danger @endif" title="{{ $buildings[ "clay" ][ "name" ] }}" >
+                                                    <div class="col-12 col-sm-4 col-lg-2" title="{{ $buildings[ "clay" ][ "name" ] }}" >
                                                         <img src="{{ asset( "assets/graphic/buildings/icons/{$buildings[ "clay" ][ "key" ]}.png" ) }}" alt="{{ $buildings[ "clay" ][ "name" ] }}" >
                                                         {{ $building[ "clay"       ] }}
                                                     </div>
-                                                    <div class="col-2 @if( $building[ "iron" ] > 100 ) text-danger @endif" title="{{ $buildings[ "iron" ][ "name" ] }}" >
+                                                    <div class="col-12 col-sm-4 col-lg-2" title="{{ $buildings[ "iron" ][ "name" ] }}" >
                                                         <img src="{{ asset( "assets/graphic/buildings/icons/{$buildings[ "iron" ][ "key" ]}.png" ) }}" alt="{{ $buildings[ "iron" ][ "name" ] }}" >
                                                         {{ $building[ "iron"       ] }}
                                                     </div>
-                                                    <div class="col-2 @if( $building[ "pop"  ] > 100 ) text-danger @endif" title="Tempo" >
-                                                        <img src="{{ asset( "assets/graphic/buildings/icons/time.png" ) }}" alt="Tempo" >
+                                                    <div class="col-12 col-sm-4 col-lg-2" title="População" >
+                                                        <img src="{{ asset( "assets/graphic/buildings/icons/pop.png" ) }}" alt="População" >
                                                         {{ $building[ "pop"        ] }}
                                                     </div>
-                                                    <div class="col-2" title="População" >
-                                                        <img src="{{ asset( "assets/graphic/buildings/icons/pop.png" ) }}" alt="População" >
+                                                    <div class="col-12 col-sm-8 col-lg-4" title="Tempo" >
+                                                        <img src="{{ asset( "assets/graphic/buildings/icons/time.png" ) }}" alt="Tempo" >
                                                         {{ $building[ "build_time" ] }}
                                                     </div>
                                                 </div>
@@ -99,7 +99,7 @@
                         <div class="table-responsive" >
                             <table id="not-build" class="table table-hover table-sm align-middle mt-5 mb-0" >
                                 <thead>
-                                    <tr>
+                                    <tr class="text-center" >
                                         <th>Ainda não disponível</th>
                                         <th>Requerimentos</th>
                                     </tr>
@@ -109,10 +109,10 @@
                                         <tr>
                                             <td>
                                                 <div class="row" >
-                                                    <div class="col-4 text-center m-auto" >
+                                                    <div class="col-12 col-lg-5 text-center m-auto" >
                                                         <img src="{{ asset( "assets/graphic/buildings/{$key}1.png" ) }}" alt="{{ $building[ "name" ] }}" >
                                                     </div>
-                                                    <div class="col-8 ps-0 m-auto" >
+                                                    <div class="col-12 col-lg-7 text-center text-lg-start ps-lg-0 m-auto" >
                                                         <p class="mb-0" >
                                                             {{ $building[ "name" ] }}
                                                         </p>
