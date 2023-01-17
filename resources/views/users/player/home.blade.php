004bca33058b978b2dfe945022ba91d98b436421
CostaMateus <costa.mack95@gmail.com>
2023-01-17 13:18:53 -0300

    fix: table fixed head

--- resources/views/users/player/home.blade.php
+++ resources/views/users/player/home.blade.php
@@ -9,17 +9,17 @@
                 <div class="card border-0" >
                     <div class="card-body" >
 
-                        <div class="table-responsive" >
+                        <div class="table-responsive table-fixed " >
                             <table id="villages" class="table table-hover table-sm align-middle mb-0" >
-                                <thead>
+                                <thead class="table-head" >
                                     @php
                                         $arrs = [ "main", "barracks", "stable", "workshop", "smithy", "farm", "market", ];
                                     @endphp
                                     <tr>
-                                        <th >Total: {{ $villages->count() }}</th>
+                                        <th scope="col" >Total: {{ $villages->count() }}</th>
                                         @foreach ( $buildings as $key => $building )
                                             @if ( in_array( $key, $arrs ) )
-                                                <th class="text-center" title="{{ $building[ "name" ] }}" >
+                                                <th scope="col" class="text-center" title="{{ $building[ "name" ] }}" >
                                                     <img width="15" height="15" src="{{ asset( "assets/graphic/buildings/icons/{$key}.png" ) }}" alt="{{ $building[ "name" ] }}"  >
                                                 </th>
                                             @endif
@@ -27,7 +27,7 @@
 
                                         @foreach ( $units as $key => $unit )
                                             @if ( $key != "militia" )
-                                                <th class="text-center" title="{{ $unit[ "name" ] }}" >
+                                                <th scope="col" class="text-center" title="{{ $unit[ "name" ] }}" >
                                                     <img width="15" height="15" src="{{ asset( "assets/graphic/units/icons/{$key}.png" ) }}" alt="{{ $unit[ "name" ] }}" >
                                                 </th>
                                             @endif
@@ -37,11 +37,11 @@
                                 <tbody>
                                     @foreach ( $villages as $id => $village )
                                         <tr class="align-middle" >
-                                            <td class="d-flex align-items-center h-100 py-1" >
+                                            <th scope="row" >
                                                 <a class="btn btn-sm btn-link text-black text-decoration-none" href="{{ route( "village.overview", [ "village" => $village ] ) }}" >
                                                     {{ $village->name }}
                                                 </a>
-                                            </td>
+                                            </th>
 
                                             {{-- edificio principal --}}
                                             <td class="text-center" >
