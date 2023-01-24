4ca55820097dd0d815ef181d1f2470c4b755f7e9
CostaMateus <costa.mack95@gmail.com>
2023-01-23 22:52:54 -0300

    add: pusher no front

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -136,5 +136,40 @@
             &copy; {{ date( "Y" ) }} <strong><span>CostaMateus</span></strong>. Todos direitos reservados.
         </footer>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://js.pusher.com/8.0/pusher.min.js"></script>
+    <script>
+        let a_tok = document.querySelector('meta[name="csrf-token"]').content;
+
+        //suscribing to pusher channel
+        Pusher.logToConsole = false;
+        var pusher = new Pusher( "123123", {
+            broadcaster: 'pusher',
+            key: "{{ env( "PUSHER_APP_KEY" ) }}",
+            cluster: "{{ env( "PUSHER_APP_CLUSTER" ) }}",
+            forceTLS: false,
+            wsHost: window.location.hostname,
+            wsPort: 6001,
+        });
+
+        var channel = pusher.subscribe( "events" );
+        channel.bind( "App\\Events\\RealTimeMessage", ( d ) => {
+
+            let id   = parseInt( "{{ $village->id }}" );
+            let data = JSON.parse( d.data );
+
+            if ( data.village.id == id )
+            {
+                let w = parseInt( data.village.stored_wood );
+                let c = parseInt( data.village.stored_clay );
+                let i = parseInt( data.village.stored_iron );
+
+                $( "#stored_wood span" ).text( w );
+                $( "#stored_clay span" ).text( c );
+                $( "#stored_iron span" ).text( i );
+            }
+        } );
+    </script>
 </body>
 </html>
