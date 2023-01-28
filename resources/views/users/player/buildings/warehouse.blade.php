31afc43f208579a0df4ae5d62b93497de4ac7ff7
CostaMateus <costa.mack95@gmail.com>
2023-01-28 10:22:45 -0300

    add: armazen capacidade

--- resources/views/users/player/buildings/warehouse.blade.php
+++ resources/views/users/player/buildings/warehouse.blade.php
@@ -17,7 +17,34 @@
                         @include( "users/player/partials.building-description", [ "building" => $buildings[ "warehouse" ] ] )
 
                         @if ( $village->building_warehouse > 0 )
-                            <p>ok</p>
+                            {{-- capacidade --}}
+                            <div class="col-12 col-xl-9 mx-auto" >
+                                <div class="table-responsive" >
+                                    <table id="builded" class="table table-hover table-sm align-middle mb-0" >
+                                        <thead>
+                                            <tr>
+                                                <th>Capacidade</th>
+                                                <th>Nível atual</th>
+                                                <th>Próximo nível</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td class="border-bottom-0" >
+                                                    <img width="15" src="{{ asset( "assets/graphic/buildings/icons/{$buildings[ "warehouse" ][ "key" ]}.png" ) }}" alt="{{ $buildings[ "warehouse" ][ "name" ] }}" >
+                                                    Capacidade atual
+                                                </td>
+                                                <td class="border-bottom-0" >
+                                                    {{ ( int ) ( $buildingsOn[ "warehouse" ][ "capacity" ] ) }}
+                                                </td>
+                                                <td class="border-bottom-0" >
+                                                    {{ ( int ) ( $buildingsOn[ "warehouse" ][ "capacity" ] * $buildingsOn[ "warehouse" ][ "capacity_factor" ] ) }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         @else
                             @if ( !empty( $buildings[ "warehouse" ][ "required" ] ) )
                                 @include( "users/player/partials.building-require", [ "name" => $buildings[ "warehouse" ][ "key" ] ] )
