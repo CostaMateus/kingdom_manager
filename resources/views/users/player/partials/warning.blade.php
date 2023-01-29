29f59a7e59651541a93edf531317231b69f698d4
CostaMateus <costa.mack95@gmail.com>
2023-01-29 16:26:39 -0300

    add: parte warning

---
+++ resources/views/users/player/partials/warning.blade.php
@@ -0,0 +1,8 @@
+
+    <div class="row mt-4" >
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto text-center" >
+            <div class="alert alert-danger bg-danger text-white" role="alert" >
+                {{ $warning_text }}
+            </div>
+        </div>
+    </div>
