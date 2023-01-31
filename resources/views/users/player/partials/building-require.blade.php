7ce0d84c63c47b2ae655bc1dfde50aadd3408dfb
CostaMateus <costa.mack95@gmail.com>
2023-01-31 10:20:20 -0300

    fix: ajuste pra usar nova var

--- resources/views/users/player/partials/building-require.blade.php
+++ resources/views/users/player/partials/building-require.blade.php
@@ -9,8 +9,8 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @if ( $b = isset( $buildingsOn[ $name ] ) ? $buildingsOn[ $name ] : $buildingsOff[ $name ] )
-                                            @foreach ( $b[ "required" ] as $key => $level )
+                                        @if ( $b = ( property_exists( $village->on, $name ) ) ? $village->on->$name : $village->off->$name )
+                                            @foreach ( $b->required as $key => $level )
                                                 <tr>
                                                     <td class="@if ( $loop->last ) border-bottom-0 @endif" >
                                                         @php
@@ -19,8 +19,8 @@
                                                             $img      = "{$key}{$png}.png";
                                                         @endphp
                                                         <button class="btn btn-link btn-sm m-auto text-decoration-none text-dark cursor-none {{ $disabled }}" >
-                                                            <img src="{{ asset( "assets/graphic/buildings/{$img}" ) }}" alt="{{ $buildings[ $key ][ "name" ] }}" >
-                                                            {{ $buildings[ $key ][ "name" ] }} - Nível {{ $level }}
+                                                            <img src="{{ asset( "assets/graphic/buildings/{$img}" ) }}" alt="{{ $b->name }}" >
+                                                            {{ $b->name }} - Nível {{ $level }}
                                                         </button>
                                                     </td>
                                                 </tr>
