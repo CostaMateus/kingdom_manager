2fdaf76c8b95da664a011e6cb3abd170935d8c0a
CostaMateus <costa.mack95@gmail.com>
2023-02-04 07:06:21 -0300

    add: doc tw

---
+++ _docs/tw-js/game.pt_BR.js
@@ -0,0 +1,6 @@
+/**** initialize constants ****/
+TribalWars.constants = {
+    item_types: { "1": "Funcionalidade", "2": "Consum\u00edvel", "3": "Passivo" },
+    item_categories: { "5": "Item de aldeia", "6": "Item de recrutamento", "4": "Item de unidade", "3": "Item do evento", "2": "Pacote de recurso", "1": "Premium" },
+    item_tags: { rarity: ["Nenhum", "Comum", "Incomum", "Raro", "Lend\u00e1rio"], use_type: ["Nenhum", "Consum\u00edvel", "Presente\u00e1vel"] },
+};
