e3a8112026ef03e4c967d8572ff23714588e0934
CostaMateus <costa.mack95@gmail.com>
2023-03-17 12:45:11 -0300

    fix: removendo import jquery local

--- resources/views/users/player/partials/update-resources.blade.php
+++ resources/views/users/player/partials/update-resources.blade.php
@@ -1,7 +1,6 @@
 
 @section( "js" )
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         window.onload = function() {
             let max    = parseFloat( "{{ $village->buildings->on->warehouse->capacity }}" );
