ea850595bedf97fd5a94d68b36cf802db1944b66
CostaMateus <costa.mack95@gmail.com>
2023-04-14 07:33:01 -0300

    fix: separando rotas q n sao do edificios

--- routes/web.php
+++ routes/web.php
@@ -107,11 +107,16 @@ Route::middleware( [ "auth" ] )->group( function () {
             Route::get( "/wall",       [ VillageController::class, "wall"       ] )->name( "wall"       );
             Route::get( "/watchtower", [ VillageController::class, "watchtower" ] )->name( "watchtower" );
 
+        } );
+
+        Route::name( "village." )->prefix( "/village/{village}/" )->group( function () {
+
             Route::post( "/unit/train/",               [ VillageController::class, "trainUnit"             ] )->name( "train.unit"              );
             Route::post( "/unit/research/{unit}",      [ VillageController::class, "researchUnit"          ] )->name( "research.unit"           );
             Route::post( "/upgrade/{building}",        [ VillageController::class, "upgradeBuilding"       ] )->name( "upgrade.building"        );
             Route::post( "/upgrade/{building}/cancel", [ VillageController::class, "cancelUpgradeBuilding" ] )->name( "cancel.upgrade.building" );
             Route::post( "/changeName",                [ VillageController::class, "changeVillageName"     ] )->name( "change.name"             );
+
         } );
     } );
 
