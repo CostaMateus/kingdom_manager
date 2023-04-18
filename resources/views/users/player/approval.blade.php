1dec0da6b4bcee7630dc84643682741ed952b9e4
CostaMateus <costa.mack95@gmail.com>
2023-04-18 15:17:24 -0300

    fix: ajuste tela de aguardando aprovação

--- resources/views/users/player/approval.blade.php
+++ resources/views/users/player/approval.blade.php
@@ -3,8 +3,8 @@
 @section( "content" )
     <div class="container" >
         <div class="row justify-content-center" >
-            <div class="col-md-8" >
-                <div class="card" >
+            <div class="col-md-6" >
+                <div class="card text-center" >
                     <div class="card-header" >Esperando aprovação</div>
 
                     <div class="card-body" >
