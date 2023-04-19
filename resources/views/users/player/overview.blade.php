3a3b5090c1fd042372a34454c24b9ac13d8e4f59
CostaMateus <costa.mack95@gmail.com>
2023-04-19 18:32:07 -0300

    fix: nova tela geral da aldeia

--- resources/views/users/player/overview.blade.php
+++ resources/views/users/player/overview.blade.php
@@ -11,47 +11,58 @@
             <div class="col-10 mx-auto" >
                 <div class="card border-0" >
 
-                    {{--
-                        /**
-                         * TODO redesenhar essa tela, ta feio
-                         */
-                    --}}
-
-                    <div class="card-body" >
+                    <div class="card-body px-3" >
                         <div class="row" >
+
                             {{-- edificios --}}
-                            <div class="col-12 col-sm-6" >
-                                <div class="table-responsive" >
-                                    <table id="overview-buildings" class="table table-hover table-sm align-middle mb-3 mb-sm-0" >
-                                        <thead>
-                                            <tr>
-                                                <th scope="col" >Edifícios</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ( $buildings as $key => $building )
-                                                <tr class="align-middle" >
-                                                    <td class="d-flex align-items-center h-100 py-1" title="{{ $building[ "name" ] }}" >
-                                                        <img src="{{ asset( "assets/graphic/buildings/{$key}1.png" ) }}" alt="{{ $building[ "name" ] }}" >
-                                                        <a class="btn btn-link text-black text-decoration-none"
-                                                            href="{{ route( "village.{$key}", [ "village" => $village ]) }}" >
-                                                            {{ $building[ "name" ] }}
-                                                            <span class="text-muted" >
-                                                                @if ( $village->{"building_{$key}"} == 0 )
-                                                                    (Não construído)
-                                                                @else
-                                                                    (Nível {{ $village->{"building_{$key}"} }})
-                                                                @endif
-                                                            </span>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                            <div class="col-12 col-md-8" >
+                                <div class="row" >
+                                    <div class="col-12 text-center" >
+                                        <p class="fw-bold h3 mb-0" >Edifícios</p>
+                                    </div>
+
+                                    @foreach ( $village->buildings->on as $key => $building )
+                                        @php
+                                            $class = "";
+
+                                            if ( in_array( $loop->iteration, [ 1, 4, 7, 10, 13, 16 ] ) )
+                                                $class .= " ps-2 pe-1 ps-xl-2 pe-xl-1";
+                                            elseif ( in_array( $loop->iteration, [ 3, 6, 9, 12, 15, 18 ] ) )
+                                                $class .= " ps-1 pe-2 ps-xl-1 pe-xl-2";
+                                            else
+                                                $class .= " px-1 px-xl-1";
+
+                                            $class .= ( $loop->iteration % 2 == 0 ) ? " ps-md-1 pe-md-2" : " ps-md-2 pe-md-1";
+
+                                            $png = Helper::getLevelImage( $key, $building->level );
+                                            $img = "{$key}{$png}.png";
+                                        @endphp
+
+                                        <div class="col-4 col-md-6 col-xl-4 my-1 {{ $class }}" title="{{ $building->name }}" >
+                                            <a class="text-decoration-none text-dark" href="{{ route( "village.{$key}", [ "village" => $village ]) }}" >
+                                                <div class="small-box h-100 d-flex flex-column justify-content-center border border-dark-subtle border-opacity-10 rounded" >
+                                                    <img class="mx-auto" src="{{ asset( "assets/graphic/buildings/resized/{$img}" ) }}" alt="{{ $building->name }}" >
+                                                    <div class="mx-auto text-center p-2" >
+                                                        <h5 class="mb-0" >
+                                                            {{ $building->name }}
+                                                        </h5>
+                                                        <span class="text-muted small" >
+                                                            @if ( $village->{"building_{$key}"} == 0 )
+                                                                (Não construído)
+                                                            @else
+                                                                (Nível {{ $village->{"building_{$key}"} }})
+                                                            @endif
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
-                            <div class="col-12 col-sm-6" >
+
+                            {{-- producao / exército / lealdade --}}
+                            <div class="col-12 col-md-4" >
                                 <div class="row" >
 
                                     {{-- producao --}}
